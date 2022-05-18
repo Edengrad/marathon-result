1fd6d487840e49ca5f01eea743584da7b888e9d3
Vadim Makarov <gitlabs541@gmail.com>
2022-05-18 09:15:30 +0300

    Реализовано чтение данных из файла

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 using namespace std;
+#include "book_resullt.h"
+#include "file_reader.h"
+#include "constants.h"
 int main()
 {
-	cout << "Laboratory work #1. GIT\n";
-	cout << "Variant #1. Marathon of result\n";
-	cout << "Author: Vadim Makarov\n";
-	cout << "Group: 14h\n";
+	setlocale(LC_ALL, "Russian");
+	cout << "Лабораторная работа №1. GIT\n";
+	cout << "Вариант №1. Результаты марафона\n";
+	cout << "Автор: Вадим Макаров\n\n";
+	book_result* results[MAX_FILE_ROWS_COUNT];
+	int size;
+	try
+	{
+		read("data.txt", results, size);
+		for (int i = 0; i < size; i++)
+		{
+			cout << results[i]->participant.number << '\n';
+			cout << results[i]->participant.last_name << '\n';
+			cout << results[i]->participant.first_name << '\n';
+			cout << results[i]->participant.middle_name << '\n';
+			cout << results[i]->finish.hour << ' ';
+			cout << results[i]->finish.minut << ' ';
+			cout << results[i]->finish.second << '\n';
+			cout << results[i]->start.hour << ' ';
+			cout << results[i]->start.minut << ' ';
+			cout << results[i]->start.second << '\n';
+			cout << results[i]->club << '\n';
+			cout << '\n';
+		}
+		for (int i = 0; i < size; i++)
+		{
+			delete results[i];
+		}
+	}
+	catch (const char* error)
+	{
+		cout << error << '\n';
+	}
 	return 0;
 }
