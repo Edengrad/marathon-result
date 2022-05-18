c65ecd007ba08cde6390dd24b1dcaec43951e7cc
Edengrad <105769417+Edengrad@users.noreply.github.com>
2022-05-18 08:29:19 +0300

    Добавлен вывод общей информации о задании

---
+++ main-project/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "Laboratory work #1. GIT\n";
+cout << "Variant #1. Marathon of result\n";
+cout << "Author: Vadim Makarov\n";
+cout << "Group: 14h\n";
+return 0;
+}
